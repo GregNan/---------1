4499110ea95442e231255c253e519d896447a286
GregNan <ananev9295dt@gmail.com>
2022-07-10 21:28:53 +0300

    задача на чётное

--- M1/Program.cs
+++ M1/Program.cs
@@ -7,11 +7,36 @@
     //Console.Write(max);
  
  //большее из 3-х
-  int a=1;
-  int c=10;
-  int b=5;
-  int max=0;
-    if (a>max) max=a; 
-    if (b>max) max=b;
-    if (c>max) max=c;
-    Console.Write(max);
+//  int a=1;
+ // int c=10;
+  //int b=5;
+  //int max=0;
+    //if (a>max) max=a; 
+    //if (b>max) max=b;
+    //if (c>max) max=c;
+    //Console.Write(max);
+//int a=5;
+//int c;
+//double b,d;
+//Console.Write("vvedite a");
+//Console.Read(a);
+//a=b;
+//c=a/2;
+//d=b/2;
+//a%2 !=0;
+//if (c>d)
+//Console.Write("Nechetnoe");
+//a%2 ==0;
+//if (c=d)
+//Console.Write("Chetnoe");
+Console.WriteLine("Введите число:");
+int a = Console.Read();
+   { if ((a%2)==0)
+       Console.WriteLine("Четное число");
+    else 
+        Console.WriteLine("Нечетное число");    
+    }
+Console.ReadLine();
+
+
+
