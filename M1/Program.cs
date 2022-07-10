9a36e71ef2b73b58da1bef8ffbcbfaf8b467ae1c
GregNan <ananev9295dt@gmail.com>
2022-07-10 20:17:53 +0300

    iz 3-h max

--- M1/Program.cs
+++ M1/Program.cs
@@ -1,8 +1,17 @@
 ﻿//Большее из двух чисел
-int a=1;
-int b=5;
-int max=0;
-    if (a>b) max=a; 
-    if (a<b) max=b;
+//int a=1;
+//int b=5;
+//int max=0;
+    //if (a>b) max=a; 
+    //if (a<b) max=b;
+    //Console.Write(max);
+ 
+ //большее из 3-х
+  int a=1;
+  int c=10;
+  int b=5;
+  int max=0;
+    if (a>max) max=a; 
+    if (b>max) max=b;
+    if (c>max) max=c;
     Console.Write(max);
-    
